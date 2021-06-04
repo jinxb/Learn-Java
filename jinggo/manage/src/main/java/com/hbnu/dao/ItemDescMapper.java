88ba4566950c93b7effaa23f999f7896570dee5b
jinxb <1659340302@qq.com>
2021-06-04 22:26:12 +0800

    删除jinggo文件夹

--- jinggo/manage/src/main/java/com/hbnu/dao/ItemDescMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hbnu.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.hbnu.pojo.ItemDesc;
-
-public interface ItemDescMapper extends BaseMapper<ItemDesc> {
-}
--- jinggo/manage/src/main/java/com/hbnu/service/FileService.java
+++
@@ -1,8 +0,0 @@
-package com.hbnu.service;
-
-import com.hbnu.vo.ImageVo;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileService {
-    ImageVo upload(MultipartFile uploadFile);
-}
